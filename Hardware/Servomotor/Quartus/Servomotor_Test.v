f6a8d741ffd098e6dcaeaaba779eb83a8fa64e27
KazumaAK <Oriverosp@unal.edu.co>
2021-05-16 13:37:22 -0500

    Adicion del Servomotor

---
+++ Hardware/Servomotor/Quartus/Servomotor_Test.v
@@ -0,0 +1,22 @@
+module Servomotor_Test(
+	input Clock,
+	input [2:0] Switches,
+	output servomotor
+);
+
+reg [11:0] ciclo=0;
+
+always @(posedge Clock)begin 
+
+	if(Switches[0]==1'b1)ciclo=12'd99;
+	else if(Switches[1]==1'b1)ciclo=12'd299;
+	else if(Switches[2]==1'b1)ciclo=12'd499;
+
+end
+
+
+Servomotor xD(Clock, 16'd249, ciclo, servomotor);
+
+
+
+endmodule
